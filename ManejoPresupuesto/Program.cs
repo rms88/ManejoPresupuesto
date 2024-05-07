fecb802ef440334d330456bba62c4b144190f40c
RICARDO MORERA SOTO <ricardo.soto@uhispano.ac.cr>
2024-05-07 11:56:56 -0600

    Clase 134

--- ManejoPresupuesto/Program.cs
+++ ManejoPresupuesto/Program.cs
@@ -7,7 +7,9 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddTransient<IRepositorioTiposCuentas, RepositorioTiposCuentas>();
 builder.Services.AddTransient<IServicioUsuarios, ServicioUsuarios>();
 builder.Services.AddTransient<IRepositorioCuentas, RepositorioCuentas>();
+builder.Services.AddTransient<IRepositorioCategorias, RepositorioCategorias>();
 builder.Services.AddAutoMapper(typeof(Program));
+
 
 var app = builder.Build();
 
